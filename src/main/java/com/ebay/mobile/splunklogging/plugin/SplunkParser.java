da3feb8be06f61ec9d1c7e1f7af143724044104b
Matthew Borkow <mborkow@ebay.com>
2014-01-27 15:44:06 -0800

    switching from top of file headers to in-line name/value pairs

--- src/main/java/com/ebay/mobile/splunklogging/plugin/SplunkParser.java
+++ src/main/java/com/ebay/mobile/splunklogging/plugin/SplunkParser.java
@@ -120,10 +120,10 @@ public class SplunkParser extends AbstractMojo {
                         // do nothing...there might not be a description
                     }
                     //String theHeaders = "class,method,status,duration,start,end,description,parameters,exception,exception-message,reporter-output";
-                    buf.append("class=" + className + ",method=" + name + ",status=" + status + ",duration=" + duration + ",start=" + started + ",end=" + ended + ",description=" + description);
+                    buf.append("class=\"" + className + "\",method=\"" + name + "\",status=\"" + status + "\",duration=\"" + duration + "\",start=\"" + started + "\",end=\"" + ended + ",description=\"" + description);
                     parseNode(aNode);
                     // we should now have all the data for our test-method and can prepare it for logging...
-                    buf.append(",parameters=" + parameterString + ",exception=" + exceptionName + ",exception-message=" + exceptionMessage + ",reporter-output=" + reporterTextString);
+                    buf.append("\",parameters=\"" + parameterString + "\",exception=\"" + exceptionName + "\",exception-message=\"" + exceptionMessage + "\",reporter-output=\"" + reporterTextString + "\"");
                     getLog().info("Parsing results: " + buf.toString());
                     theOutput.add(buf.toString());
                     buf = new StringBuffer();
@@ -225,15 +225,19 @@ public class SplunkParser extends AbstractMojo {
 
         for (String line : theOutput) {
             getLog().info("Adding to CSV output: " + line);
+            out.write(line);
+            out.newLine();
+            /*
             String[] lineArray = line.split(",");
             for (String s : lineArray) {
                 System.out.println(s);
             }
+             */
 
-            writer.writeNext(lineArray);
+            //writer.writeNext(lineArray);
         }
 
-        writer.close();
+        //writer.close();
     }
     
     public void execute () throws MojoExecutionException {
