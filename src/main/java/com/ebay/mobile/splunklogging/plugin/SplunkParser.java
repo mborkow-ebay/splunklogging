95074b827568c9d9b8e372946a4900a8bf1f3738
Matthew Borkow <mborkow@ebay.com>
2014-01-27 16:20:37 -0800

    fixing missing trailing quote for end value pair

--- src/main/java/com/ebay/mobile/splunklogging/plugin/SplunkParser.java
+++ src/main/java/com/ebay/mobile/splunklogging/plugin/SplunkParser.java
@@ -126,10 +126,10 @@ public class SplunkParser extends AbstractMojo {
                         // do nothing...there might not be a description
                     }
                     //String theHeaders = "class,method,status,duration,start,end,description,parameters,exception,exception-message,reporter-output";
-                    buf.append("class=\"" + className + "\",method=\"" + name + "\",status=\"" + status + "\",duration=\"" + duration + "\",start=\"" + started + "\",end=\"" + ended + "\",description=\"" + description);
+                    buf.append("class=\"" + className + "\", method=\"" + name + "\", status=\"" + status + "\", duration=\"" + duration + "\", start=\"" + started + "\", end=\"" + ended + "\", description=\"" + description);
                     parseNode(aNode);
                     // we should now have all the data for our test-method and can prepare it for logging...
-                    buf.append("\",parameters=\"" + parameterString + "\",exception=\"" + exceptionName + "\",exception-message=\"" + exceptionMessage + "\",reporter-output=\"" + reporterTextString + "\"");
+                    buf.append("\", parameters=\"" + parameterString + "\", exception=\"" + exceptionName + "\", exception-message=\"" + exceptionMessage + "\", reporter-output=\"" + reporterTextString + "\"");
                     getLog().info("Parsing results: " + buf.toString());
                     theOutput.add(buf.toString());
                     buf = new StringBuffer();
